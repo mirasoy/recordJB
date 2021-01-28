edb180920fc24d4c5e01c31476b20db16385c365
mirasoy <nkskjjbb@gmail.com>
2021-01-29 08:58:44 +0900

    fix : build setting

--- src/main/java/recordJB/RecordJbApplication.java
+++ src/main/java/recordJB/RecordJbApplication.java
@@ -3,7 +3,7 @@ package recordJB;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class RecordJbApplication {
 
 	public static void main(String[] args) {
--- src/main/java/recordJB/ServletInitializer.java
+++ src/main/java/recordJB/ServletInitializer.java
@@ -1,13 +1,20 @@
 package recordJB;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	public static void main(String[] args) {
+		SpringApplication.run(ServletInitializer.class, args);
+	}
+
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(RecordJbApplication.class);
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServletInitializer.class);
 	}
 
 }
